42a47f748845a38602e61031dafba5da6dddb75f
thangvu2325 <thangvu2325@gmail.com>
2023-01-25 18:23:42 +0700

    update

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,38 +1,7 @@
-import React, { useState } from 'react';
-
-const courses = [
-    {
-        id: 1,
-
-        name: 'ReactJS',
-    },
-    {
-        id: 2,
-
-        name: 'JavaScrip',
-    },
-    {
-        id: 3,
-
-        name: 'CSS',
-    },
-];
-
 function App() {
-    const [checked, setChecked] = useState(2);
-    // console.log(checked)
-    const handleSubmit = () => {
-        console.log({ id: checked });
-    };
     return (
-        <div style={{ padding: 32 }}>
-            {courses.map((course) => (
-                <div key={course.id}>
-                    <input type="checkbox" checked={checked === course.id} onChange={() => setChecked(course.id)} />
-                    {course.name}
-                </div>
-            ))}
-            <button onClick={handleSubmit}>Register</button>
+        <div className="App">
+            <h1>Hello World</h1>
         </div>
     );
 }
